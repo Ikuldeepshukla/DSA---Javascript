4b2c100edf16d0f4bba62ed9ecf4a61f62c979cb
Kuldeep <kuldeepshukla09496@gmail.com>
2023-03-10 01:12:40 +0530

    solution - Cube Sum Pairs

---
+++ Array/codingNinjasProblems/cubeSumPairs.js
@@ -0,0 +1,29 @@
+// Cube Sum Pairs - https://www.codingninjas.com/codestudio/problems/cube-sum-pairs_1060717?topList=top-array-coding-interview-questions
+// problem statement - You are given a positive integer N, and you have to find the number of ways to represent N
+// as a sum of cubes of two integers (let's say A and B), such that:
+// N = A^3 + B^3
+
+const findCubeSumPairs = (num) => {
+  let result = [];
+  for (let i = 0; i < num; i++) {
+    for (let j = 0; j < num; j++) {
+      console.log(i, j);
+      console.log(Math.pow(i, 3) + Math.pow(j, 3));
+      if (Math.pow(i, 3) + Math.pow(j, 3) === num) {
+        result = [...result, { i, j }];
+      }
+      if (Math.pow(j, 3) > num) {
+        break;
+      }
+    }
+    if (Math.pow(i, 3) > num) {
+      break;
+    }
+  }
+  return result.length;
+};
+
+// driver code
+let num = 72;
+
+console.log(findCubeSumPairs(num));
